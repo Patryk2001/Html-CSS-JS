42284a3b163b6abb8bdcf9ff972a8c8e633f21cd
Patryk <patrykvip1@o2.pl>
2019-01-29 11:24:21 +0100

    Add files via upload

---
+++ HTML/index.js
@@ -0,0 +1,14 @@
+// Color variables:
+red = [0, 100, 63];
+orange = [40, 80, 60];
+green = [75, 100, 40];
+blue = [200, 77, 55];
+purple = [280, 50, 20];
+
+// Letters in the message will cycle through these colors:
+letterColors = [green];
+
+message = 'Patryk Madzia';
+
+drawName(message, letterColors);
+bounceBubbles();
